5605ef1d257cc2e24ddf9de4ce058c1b15e53540
alfarioekaputra <alfarioekaputra@gmail.com>
2018-01-13 11:00:17 +0700

    untuk pertanyaan 2 & 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import io from "socket.io-client";
 
 class App extends Component {
   constructor(props) {
@@ -10,6 +11,19 @@ class App extends Component {
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.socket = io('localhost:8080');
+
+    this.socket.on('RECEIVE_MESSAGE', function(data){
+      console.log(data);
+      addMessage(data);
+    });
+
+    const addMessage = data => {
+        console.log(data);
+        this.setState({messages: [...this.state.messages, data]});
+        console.log(this.state.messages);
+    };
   }
 
   handleInputChange(event) {
@@ -19,6 +33,7 @@ class App extends Component {
   }
 
   renderMessages() {
+    console.log(this.state.messages);
     const { messages } = this.state;
     if (messages.length > 0) {
       return messages.map(message => {
@@ -33,14 +48,45 @@ class App extends Component {
   }
 
   handleSubmit(event) {
+    const url = 'https://newsapi.org/v2/everything?q=pariwisata indonesia&apiKey=be060bcb81814c4d89d8db3c67142271'; // Get 10 random users
+    fetch(url)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          this.setState({
+            input: '',
+            messages: [
+              ...this.state.messages,
+              responseJson.articles.title,
+            ]
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    /*const url = 'https://newsapi.org/v2/everything?q=pariwisata indonesia&apiKey=be060bcb81814c4d89d8db3c67142271'; // Get 10 random users
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
+.then(function(response) {
+    console.log(response);
+    // Your code for handling the data you get from the API
     this.setState({
       input: '',
       messages: [
         ...this.state.messages,
-        this.state.input,
+        response,
       ]
     });
+})
+.catch(function() {
+    // This is where you run code if the server returns any errors
+    console.log('aaa');
+});*/
+
+    
     event.preventDefault();
+
+    this.socket.emit('SEND_MESSAGE', {
+        input: this.state.input,
+    })
   }
 
   render() {
